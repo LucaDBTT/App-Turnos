d10ab2018f3fa757f24033d9bf4884fca3a67548
nicolas hernandez <101599191+nicccooh@users.noreply.github.com>
2023-11-29 13:40:25 -0300

    delete Logeado.aspx/comienzo MisTurnos

--- Turnos/Login.aspx.cs
+++ Turnos/Login.aspx.cs
@@ -28,7 +28,19 @@ namespace Turnos
                 if (negocio.loguear(usuario))// con el metodo vemos si es correcto lo ingresado y si es correcto en este caso lo mando a default
                 {
                     Session.Add("usuario", usuario);
-                    Response.Redirect("Logeado.aspx", false);
+                    //Response.Redirect("Logeado.aspx", false);
+                    if (EsAdministrador())
+                    {
+                        Response.Redirect("Default.aspx", false);
+                    }
+                    else if (EsUsuario())
+                    {
+                        Response.Redirect("UserDefault.aspx", false);
+                    }
+                    else if (EsMedico())
+                    {
+                        Response.Redirect("Default.aspx", false);
+                    }
                 }
                 else
                 {
@@ -46,6 +58,24 @@ namespace Turnos
         protected void btnRegistrarse_Click(object sender, EventArgs e)
         {
             Response.Redirect("Registrarse.aspx", false);
+        }
+
+        private bool EsAdministrador()
+        {
+            // Verifica si la sesión contiene un usuario y si ese usuario es un administrador
+            return Session["Usuario"] != null && ((Dominio.usuarios)Session["Usuario"]).TipoUsuario == Dominio.TipoUsuarios.admin;
+        }
+
+        private bool EsUsuario()
+        {
+            // Verifica si la sesión contiene un usuario y si ese usuario es un usuario regular
+            return Session["Usuario"] != null && ((Dominio.usuarios)Session["Usuario"]).TipoUsuario == Dominio.TipoUsuarios.paciente;
+        }
+
+        private bool EsMedico()
+        {
+            // Verifica si la sesión contiene un usuario y si ese usuario es un usuario regular
+            return Session["Usuario"] != null && ((Dominio.usuarios)Session["Usuario"]).TipoUsuario == Dominio.TipoUsuarios.medico;
         }
     }
 }
